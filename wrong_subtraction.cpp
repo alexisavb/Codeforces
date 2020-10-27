c20859e3de26443c7efa7cf91efb9f50271f55ed
alexisavb <alexis.osnaya.ipn@gmail.com>
2020-10-26 20:19:09 -0600

    Problem 977A - Wrong Subtraction

---
+++ wrong_subtraction.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+    while(k != 0){
+        if(n % 10 != 0){ n--; }
+        else{ n /= 10;}
+        k--;
+    }
+    cout << n << endl;
+    return 0;
+}
