49744d77ce78188029abd216cd6c3272cc6a16a9
alexisavb <alexis.osnaya.ipn@gmail.com>
2020-11-21 17:46:22 -0600

    Problem 230A - Dragons

---
+++ dragons.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int strength, dragons, dragonStrength, bonus;
+    vector<pair<int,int> > vctr;
+    cin >> strength >> dragons;
+    while(dragons-- > 0){
+        cin >> dragonStrength >> bonus;
+        vctr.push_back(pair<int,int>(dragonStrength,bonus));
+    }
+
+    sort(vctr.begin(), vctr.end());
+
+    for(int i = 0; i < vctr.size(); i++){
+         if(strength > vctr[i].first){ strength += vctr[i].second; }
+        else{ cout << "NO" << endl; return 0;}
+    }
+    cout << "YES" << endl;
+    return 0;
+}
